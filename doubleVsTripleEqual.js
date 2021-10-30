9ab94fd311461fabe420ee3f7c7e7f79b9a19a61
mnroy <85918664+mnroy@users.noreply.github.com>
2021-10-30 23:05:13 +0600

    add equal

---
+++ doubleVsTripleEqual.js
@@ -0,0 +1,19 @@
+// const num1 = 1;
+// const num2 = true;
+
+// if(num1 == num2){
+//     console.log("condition is true")
+// }
+// else{
+//     console.log("condition is false");
+// }
+
+const num1 = 1;
+const num2 = true;
+
+if(num1 === num2){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false");
+}
