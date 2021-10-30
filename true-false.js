c769f669878ced0997cd1d039e4979b51f69020e
mnroy <85918664+mnroy@users.noreply.github.com>
2021-10-30 21:31:20 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,20 @@
+let number = "";
+if(number){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+// 0 is a false;
+// Nan is a false;
+// udefined is a false;
+// "" is a false;
+// true
+// number is true = "number"
+// number is true = "0";
+
+let num = 1;
+for(num; num <= 10; num++){
+    console.log(num);
+}
