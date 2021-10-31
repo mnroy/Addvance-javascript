df14883d8dd1d4b5faa71c3b3a8631f3143ad902
mnroy <85918664+mnroy@users.noreply.github.com>
2021-10-31 10:21:21 +0600

    closure done

---
+++ closure.js
@@ -0,0 +1,19 @@
+function stopWatch(){
+    let count = 0;
+    return function(){
+        return count++;
+    }
+}
+const clock1 = stopWatch();
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+
+const clock2 = stopWatch();
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
+console.log(clock1());
+console.log(clock2());
